12988b70eb7ed8d5eb4de0118211e70774124024
aoizora <aoizora@archlinux>
2019-08-14 10:28:06 +0200

    Added p2p server

--- app/index.js
+++ app/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const parser = require('body-parser');
 const Blockchain = require('../blockchain');
+const P2PServer = require('./p2p-server');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 const app = express();
 const bc = new Blockchain();
+const p2pServer = new P2PServer(bc);
 
 app.use(parser.json());
 
@@ -21,3 +23,5 @@ app.post('/mine', (req, res) => {
 app.listen(HTTP_PORT, () => {
     console.log(`Listening on port ${HTTP_PORT}`);
 });
+
+p2pServer.listen();
